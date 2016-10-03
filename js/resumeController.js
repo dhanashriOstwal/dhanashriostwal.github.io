508e83c89b6951a36a9ee12917b42d6c99cb8ddd
dhanashriOstwal <ostwal.dhanashri@gmail.com>
2016-10-02 19:51:27 -0500

    Update resumeController.js

--- js/resumeController.js
+++ js/resumeController.js
@@ -3,6 +3,7 @@ var app = angular.module("personalWebsite",[])
 app.controller("resumeCtrl",['$scope', '$http', function($scope,$http)
 	{
 		$http.get("resume.txt").success(function(data){
+			alert("Inside get");
 			$scope.resumeVar = JSON.parse(data);
 		});
 	}]
