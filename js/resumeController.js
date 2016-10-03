01ede27801dea99f20e97f95f964eca116ba9e1d
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-02 21:11:54 -0500

    final

--- js/resumeController.js
+++ js/resumeController.js
@@ -3,7 +3,7 @@ var app = angular.module("personalWebsite",[])
 app.controller("resumeCtrl", function($scope,$http)
 	{
 		
-		$http.get("js/resume.txt").success(function(response){
+		$http.get("resume.txt").success(function(response){
 			
 			
 			$scope.resumeVar = response.data;
