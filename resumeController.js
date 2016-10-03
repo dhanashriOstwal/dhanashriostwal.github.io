792c81ebe1966c2a20ea9bba256cbb3779b2257f
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-02 20:30:47 -0500

    Adding existing files

--- resumeController.js
+++ resumeController.js
@@ -2,7 +2,7 @@ var app = angular.module("personalWebsite",[])
 
 app.controller("resumeCtrl", function($scope,$http)
 	{
-		
+		$scope.fname = "Dhanashri"
 		$http.get("resume.txt").success(function(response){
 			$scope.Jsoned = angular.fromJson(response.data)
 			
