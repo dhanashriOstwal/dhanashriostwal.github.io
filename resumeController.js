3d305840fca4dab73bd5bf4718498dcc0c0be7f1
dhanashriOstwal <ostwal.dhanashri@gmail.com>
2016-10-02 20:59:01 -0500

    Update resumeController.js

--- resumeController.js
+++ resumeController.js
@@ -3,7 +3,7 @@ var app = angular.module('personalWebsite',[]);
 app.controller('resumeCtrl', function($scope,$http)
 	{
 		$scope.fname = "Dhanashri"
-		$http.get('resume.txt').success(function(response){
+		$http.get('resume.txt').then(function(response){
 			$scope.Jsoned = angular.fromJson(response.data)
 			
 			$scope.resumeVar = response.data;
