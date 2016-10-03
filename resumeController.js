1d6a6b4c596b0049a7294130b2854aa2d0bf866e
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-02 23:51:19 -0500

    fileRead2

--- resumeController.js
+++ resumeController.js
@@ -3,17 +3,18 @@ var app = angular.module("personalWebsite",[])
 app.controller("resumeCtrl", function($scope,$http)
 	{
 		
-		$http.get("resume.txt").then(function(response){
+		$http.get("resume1.txt").then(function(response){
 			
 			
 			// $scope.resumeVar = response.data;
-			var ans = ''
+			var ans = {}
 			var lines = response.data.split('\n');
 			$scope.line = lines
 			for(var line = 0; line < lines.length; line++)
 			{
-				ans = ans + lines[line];
-				ans = ans + '\n';
+					
+				 ans = ans + lines[line];
+				ans = ans + '\n'; 
 				
 			}
 			$scope.resumeVar = ans
