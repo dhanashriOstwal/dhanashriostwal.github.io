fadb2a1150197acb37b4a995789f5cabb2244a42
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-02 19:56:43 -0500

    Adding existing files

---
+++ resumeController.js
@@ -0,0 +1,12 @@
+var app = angular.module("personalWebsite",[])
+
+app.controller("resumeCtrl",['$scope', '$http', function($scope,$http)
+	{
+		$scope.fname = "Dhanashri";
+		alert("Inside controller");
+		$http.get("resume.txt").success(function(data){
+			alert("Inside get");
+			$scope.resumeVar = JSON.parse(data);
+		});
+	}]
+);
