b6f9798de52d3c64ff1d32891e87662f074750ce
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-04 00:06:03 -0500

    fileRead2

--- resumeController.js
+++ resumeController.js
@@ -39,7 +39,7 @@ app.controller("resumeCtrl", function($scope,$http)
 				//console.log(index + ': ' + value);
 				
 			});
-			console.log(json_data)
+			console.log(JSON.stringif(json_data))
 ////////////////////////////////////
 			$scope.resumeVar = json_data
 		});
