95ef2a77c2c3ab27f72dc68d0342ca87d8893b32
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-03 22:31:06 -0500

    fileRead2

--- resumeController.js
+++ resumeController.js
@@ -12,7 +12,7 @@ app.controller("resumeCtrl", function($scope,$http)
 			//$scope.line = lines.split("\\n\\n")
 			
 			
-			BufferedReader in = new BufferedReader(new FileReader("foo.txt"));
+			BufferedReader in = new BufferedReader(new FileReader("resume1.txt"));
 
 			List<String> allStrings = new ArrayList<String>();
 			String str ="";
