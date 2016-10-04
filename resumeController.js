9364e59c154285c77c79d5ed498604a950b6ae8e
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-03 22:48:06 -0500

    fileRead2

--- resumeController.js
+++ resumeController.js
@@ -7,17 +7,21 @@ app.controller("resumeCtrl", function($scope,$http)
 		var arrNavigate = ['Home','Objective','Education','Relevant Course Work','Professional Experience','Projects','Technical Skills']	
 			
 			// $scope.resumeVar = response.data;
-			var ans = {}
+			var json_data = {}
 			var lines = response.data;
 			$scope.line = lines.split("\n")
 			
 			
 			//console.log($scope.line)
 			angular.forEach($scope.line, function(value, index) {
-				console.log(index + ': ' + value);
+				if(index <= 4){
+					json_data[index] = value
+				}
+				//console.log(index + ': ' + value);
+				console.log(json_data)
 			});
 ////////////////////////////////////
-			$scope.resumeVar = ans
+			$scope.resumeVar = json_data
 		});
 	}
 );
