5711b6939d6404b8c73aee3d15ab809f40c6d4f5
Dhanashri <dhanashrivilas.ostwal@mavs.uta.edu>
2016-10-05 21:51:59 -0500

    fileRead2

--- resumeController.js
+++ resumeController.js
@@ -75,6 +75,7 @@ app.controller("resumeCtrl", function($scope,$http)
 			$scope.mySplit = function(string) {
 				var array = string.split(',');
 				$scope.educationDetails = array;
+				console.log($scope.educationDetails);
 				return $scope.educationDetails;
 			}
 			
